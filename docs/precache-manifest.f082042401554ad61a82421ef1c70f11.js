2ad663aac8a2f1ed4cae039b113905555fbb3ceb
Iñigo <inigo@localhost.localdomain>
2021-10-29 19:28:46 +0200

    first commit

---
+++ docs/precache-manifest.f082042401554ad61a82421ef1c70f11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7b61fd3e09dafd577378ea5dfb9d86a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "092533a1ba1bb9a6c9f6",
+    "url": "/static/css/main.a6601f16.chunk.css"
+  },
+  {
+    "revision": "555f5e3d44f74d45dc48",
+    "url": "/static/js/2.a2905968.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a2905968.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092533a1ba1bb9a6c9f6",
+    "url": "/static/js/main.c7697995.chunk.js"
+  },
+  {
+    "revision": "429b012a069fc567e7f4",
+    "url": "/static/js/runtime-main.c670b3c9.js"
+  }
+]);
